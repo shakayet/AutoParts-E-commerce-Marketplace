4dc9e83df1f1e6f1557ef42130725d97289ec8d1
Shakhawat <srabonshakhawat@gmail.com>
2025-10-24 04:44:26 +0600

    Update Review zod validation

--- src/app/modules/review/review.validation.ts
+++ src/app/modules/review/review.validation.ts
@@ -4,7 +4,7 @@ const createReviewZodSchema = z.object({
   body: z.object({
     productId: z.string({ required_error: 'Product id is required' }),
     rating: z.number({ required_error: 'Rating is required' }).min(1).max(5),
-    comment: z.string().optional(),
+    comment: z.string({ required_error: 'Product review required' }),
   }),
 });
 
