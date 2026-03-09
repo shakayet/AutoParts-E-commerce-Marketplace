c762695c93b67626345faadcc86c21822c35fc99
Shakhawat <srabonshakhawat@gmail.com>
2026-03-10 02:20:45 +0600

    Update auth service

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -45,7 +45,7 @@ const loginUserFromDB = async (payload: ILoginData) => {
   }
 
   //check user status
-  if (isExistUser.status === 'delete') {
+  if (isExistUser.status === 'banned') {
     throw new ApiError(
       StatusCodes.BAD_REQUEST,
       'You don’t have permission to access this content.It looks like your account has been deactivated.',
