da48bd01fdde3751da454ac37c5903f1956c59a1
Shakhawat <srabonshakhawat@gmail.com>
2026-04-26 04:17:07 +0600

    Update user interface

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 const createUserZodSchema = z.object({
   body: z.object({
     name: z.string({ required_error: 'Name is required' }),
-    contact: z.string({ required_error: 'Contact is required' }),
+    whatsappNumber: z.string().optional(),
     email: z.string({ required_error: 'Email is required' }),
     password: z.string({ required_error: 'Password is required' }),
     location: z.string().optional(),
