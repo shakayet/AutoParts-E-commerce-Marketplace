eb2dfb2cf057032590f9db3f19ac85ab2eae496d
Shakhawat <srabonshakhawat@gmail.com>
2026-05-15 03:23:06 +0600

    fix minor bug

--- src/app/modules/category/category.service.ts
+++ src/app/modules/category/category.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-catch */
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-explicit-any */
